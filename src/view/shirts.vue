b060847d35ef4f17970c6e5b2e7b6347941d0f9b
stresbal <timleonbandsma@gmail.com>
2024-06-19 08:41:23 +0200

    plaatjes

--- src/view/shirts.vue
+++ src/view/shirts.vue
@@ -1,13 +1,13 @@
 <template>
- <div class="shirts">
-    <img src="@/assets/images/shirts1.png" alt="" />
-    <img src="@/assets/images/shirts2.png" alt="" />
-    <img src="@/assets/images/shirts3.png" alt="" />
-    <img src="@/assets/images/shirts4.png" alt="" />
-    <img src="@/assets/images/shirts5.png" alt="" />
-    <img src="@/assets/images/shirts6.png" alt="" />
-    <img src="@/assets/images/shirts7.png" alt="" />
-    <img src="@/assets/images/shirts8.png" alt="" />
+ <div class="img-box">
+    <img class="img-content" src="@/assets/images/shirts1.png" alt="" />
+    <img class="img-content" src="@/assets/images/shirts2.png" alt="" />
+    <img class="img-content" src="@/assets/images/shirts3.png" alt="" />
+    <img class="img-content" src="@/assets/images/shirts4.png" alt="" />
+    <img class="img-content" src="@/assets/images/shirts5.png" alt="" />
+    <img class="img-content" src="@/assets/images/shirts6.png" alt="" />
+    <img class="img-content" src="@/assets/images/shirts7.png" alt="" />
+    <img class="img-content" src="@/assets/images/shirts8.png" alt="" />
 
     
  </div>
